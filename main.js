2811dd8083448eb73796ae93713bdb8f6c4a8aa5
caosegueda <83197948+caosegueda@users.noreply.github.com>
2021-07-25 01:21:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwc1GTmXdTj_Ku9VXLNgdFcf21LaJ_wBcP0cd0_LnAXRr0q6Uq6RotRwFR4tV4KTszMSQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const theButton = document.getElementById("myButton")
+
+async function getIp(){
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    return data;
+};
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    //help with no double clicking
+    theButton.disabled = true;
+
+    var y = document.getElementById("txtHidden");
+
+    getIp().then(function(data){
+        let myJSON = JSON.stringify(data)
+        y.value = myJSON;
+        console.log("hello" + y.value);
+
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        //you can add zoom link here after there is a response but appears to be a delay
+        .then(response => console.log(''))
+        //consider adding zoom link anyways if error. 
+        .catch(error => console.error('Error!', error.message))
+        // add zoom link here. 
+         window.open("https://adamsfarmcongregation.org/","_top")
+    })
+});
+
+    
+ 
